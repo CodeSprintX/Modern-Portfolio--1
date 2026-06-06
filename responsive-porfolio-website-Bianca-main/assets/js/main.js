a543f862ae03af8f52063c24c2f1b13e6046c8c7
Piyush Verma <167591956+CodeSprintX@users.noreply.github.com>
2026-06-06 16:51:36 +0530

    3rd commit

--- responsive-porfolio-website-Bianca-main/assets/js/main.js
+++ responsive-porfolio-website-Bianca-main/assets/js/main.js
@@ -103,7 +103,14 @@ servicesButtons.forEach(button =>{
 
 
 /*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/ 
+const tracks = document.querySelectorAll('.testimonials__content')
+tracks.forEach(track =>{
+   const cards = [...track.children]
 
+   for(const card of cards){
+      track.appendChild(card.cloneNode(true))
+   }
+})
 
 /*=============== CONTACT EMAIL JS ===============*/ 
 
